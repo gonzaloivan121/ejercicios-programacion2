b1313c90944d426c5cf333d2f5f7e79cc41e607b
Luis José Sánchez <luisjose@espiral.io>
2017-10-05 10:38:17 +0200

    Hola Mundo mejorado

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,5 +7,6 @@
 public class HolaMundo { // Clase principal
   public static void main(String[] args) {
     System.out.println("¡Hola mundo!");
+    System.out.println("Programa realizado por Luis José");
   }
 }
